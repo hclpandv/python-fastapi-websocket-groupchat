7a4168da444fb29f54b14026b7c7c623d99f9a22
Vikas Pandey <vikas110@gmail.com>
2024-05-21 14:37:29 +0000

    updated app config

---
+++ deploy-appgw-aks-agic.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+resource_group="aks-ne-kubenet-investigation-01"
+aks="aks-ne-kubenet-investigation-01"
+application_gw_name="appgw-ne-kubenet-investigation-01"
+application_gw_public_ip="pip-${application_gw_name}"
+vnet_name="vnet-ne-kubenet-investigation-01"
+location="northeurope" 
+
+# Deploy resource group
+az group create --name $resource_group --location $location
+
+# Deploy AKS with AGIC ingress addon
+az aks create -n $aks -g $resource_group \
+    --network-plugin azure \
+    --enable-managed-identity \
+    -a ingress-appgw \
+    --appgw-name $application_gw_name \
+    --appgw-subnet-cidr "10.225.0.0/16" \
+    --generate-ssh-keys
+
+# Get application gateway id from AKS addon profile
+appGatewayId=$(az aks show -n $aks -g $resource_group -o tsv --query "addonProfiles.ingressApplicationGateway.config.effectiveApplicationGatewayId")
+
+# Get Application Gateway subnet id
+appGatewaySubnetId=$(az network application-gateway show --ids $appGatewayId -o tsv --query "gatewayIPConfigurations[0].subnet.id")
+
+# Get AGIC addon identity
+agicAddonIdentity=$(az aks show -n $aks -g $resource_group -o tsv --query "addonProfiles.ingressApplicationGateway.identity.clientId")
+
+# Assign network contributor role to AGIC addon identity to subnet that contains the Application Gateway
+az role assignment create --assignee $agicAddonIdentity --scope $appGatewaySubnetId --role "Network Contributor"
--- setup-aks-addons-dependencies.sh
+++ setup-aks-addons-dependencies.sh
@@ -3,5 +3,5 @@
 resource_group="aks-we-kubenet-investigation-01"
 aks="aks-ne-kubenet-investigation-02"
 
-# enable approuting add-on
+# enable approuting add-on to install nginx controller
 az aks approuting enable --resource-group $resource_group --name $aks
